ca2f39b840f50936269328687739cafa24d9311a
Irina Meroi <irimeroi7@gmail.com>
2024-04-15 18:19:11 -0700

    actividad 4

---
+++ 04-actividad.py
@@ -0,0 +1,33 @@
+#  Ingresar un número entero positivo A (validar el dato) y a continuación leer tantos números enteros como se ingresó en A. Se pide informar:
+# El promedio de los números leídos.
+# El porcentaje de los positivos impares.
+# El valor máximo y mínimo leído.
+numero = int(input("Ingrese un número entero positivo: "))
+totalNumeros = 0
+positivos_impares = 0
+porcentaje = 0
+maximo = 0
+minimo = float('inf')
+
+if numero <= 0:
+    print("El número debe ser mayor a 0")
+
+for num in range(numero):
+    numEnteros = int(input("Ingrese otros números enteros: "))
+    totalNumeros = totalNumeros + numEnteros
+    promedio = totalNumeros / numero
+
+    if numEnteros >= 0 and numEnteros % 2 != 0:
+        positivos_impares += 1
+        porcentaje = (numero/positivos_impares) * 100
+
+    if numEnteros > maximo:
+        maximo = numEnteros
+    if numEnteros < minimo:
+        minimo = numEnteros
+
+print(totalNumeros)
+print(f"El promedio de números es: {promedio}")
+# print(f"Cantidad de positivos impares: {positivos_impares}")
+print(f"El porcentaje de números positivos impares es: {porcentaje}%")
+print(f"El valor máximo leído es: {maximo} y el valor mínimo: {minimo}")
--- useful.py
+++ useful.py
@@ -4,3 +4,8 @@ yearlySalary = hourlySalary*40*4*12
 print(monthlySalary)
 print(yearlySalary)
 
+# primer parcial 
+# segundo parcial en python
+# 50% de actividades entregadas
+# final: 2 ejercicios similares al parcial
+# preguntas sobre el ejercicio y preguntas generales de la materia
