44710b5db5debd12a2fb312ae0ea38ce89126c41
Irina Meroi <irimeroi7@gmail.com>
2024-05-28 13:26:17 -0700

    actividad arreglos

--- 08-actividad.py
+++ 08-actividad.py
@@ -1,9 +1,34 @@
-# En base a los conceptos vistos en el módulo, debés realizar el siguiente ejercicio: 
+numeros_enteros = [40, 10, 20, 30]
+max = 0
+min = 100
+cantidad_pares = 0
+suma_pares = 0
+print(numeros_enteros)
 
-# Cargar un arreglo con 12 números enteros. Mostrarlo y calcular:
+# Calcular el promedio de los elementos ubicados en las posiciones pares
+for i in range(0, 4):
+    if i % 2 == 0 or i == 0:
+        suma_pares += numeros_enteros[i]
+        cantidad_pares += 1
+print(f"La suma de los pares es: {suma_pares}")
+print(f"La cantidad de posiciones pares es: {cantidad_pares}")
+print(f"EL promedio de los elementos ubicados en las posiciones pares es {suma_pares / cantidad_pares}")
 
-# El elemento máximo y mínimo.
-# Calcular el promedio de los elementos ubicados en posiciones pares.
-# Insertar después del elemento máximo, la mitad del mismo.
-# Mostrar el arreglo ordenado de mayor a menor.
+# Calcular max y min
+for i in range(len(numeros_enteros)):
+    if numeros_enteros[i] > max:
+        max = numeros_enteros[i]
+    if numeros_enteros[i] < min:
+        min = numeros_enteros[i]
+print(f"El maximo es: {max}")
+print(f"El minimo es: {min}")
 
+mitad_max = max / 2
+print(f"La mitad del numero maximo es: {mitad_max}")
+pos_max = numeros_enteros.index(max)
+numeros_enteros.insert(pos_max + 1, mitad_max)
+print(numeros_enteros)
+
+# Mostrar el arreglo ordenado de mayor a menor
+numeros_enteros.sort(reverse=True)
+print(f"Lista ordenada de mayor a menor: {numeros_enteros}")
